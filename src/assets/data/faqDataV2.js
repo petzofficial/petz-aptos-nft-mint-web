f3c155a07e4386caf35f41dd8f1f8df60721dfa2
Yong Loon <nyl9488.yln@gmail.com>
2024-04-04 01:10:30 +0800

    updates

--- src/assets/data/faqDataV2.js
+++ src/assets/data/faqDataV2.js
@@ -1,28 +1,29 @@
 export const data1 = [
   {
-    title: "What is PetZ NFT ?",
-    text: "PetZ NFT is a collectible NFT that runs on the blockchain. Each PetZ NFT is unique and generated randomly, meaning that no two PetZ NFTs are the same.",
+    title: "What are PetZ NFTs?",
+    text: "PetZ NFTs are unique digital collectibles built on the blockchain. Each PetZ NFT is one-of-a-kind, generated randomly with its own special characteristics.",
   },
   {
-    title: "How we can buy and invest NFT ?",
-    text: "",
+    title: "What can I do with PetZ NFTs?",
+    text: "* Collecting: Showcase and share your one-of-a-kind PetZ with others! Build your collection and admire the variety. \n* Gaming: (Future functionality) Imagine using your PetZ NFTs as playable characters in games or as special items to enhance your gameplay experience. \n* Trading: The world of PetZ NFTs is yours to explore! Trade your PetZ with other users to earn PGC (potentially the in-game currency) or acquire new and exciting PetZ additions to your collection.",
   },
   {
-    title: "Why we should choose PetZ NFT ?",
-    text: "PetZ NFTs can be used for a variety of purposes including collecting, gaming and trading.",
+    title: "What is PGC?",
+    text: "PGC might be the in-game currency used within the PetZ NFT ecosystem. More information on how to earn and utilize PGC might be available on the official PetZ NFT resources.",
   },
 ];
+
 export const data2 = [
   {
-    title: "Where we can buy and sell NFTs ?",
-    text: "",
+    title: "Where can I learn more about PetZ NFTs?",
+    text: "For comprehensive information about PetZ NFTs,  explore the official website, social media channels, or any available whitepapers or FAQs they might offer.",
   },
   {
-    title: "How secure is this token ?",
-    text: "",
+    title: "How do I buy a PetZ NFT",
+    text: "You can mint PetZ NFTs here. The buying process for PetZ NFTs might involve visiting a dedicated marketplace or platform they collaborate with.  Look for official instructions from PetZ to ensure a safe and secure purchase.",
   },
   {
-    title: "What is your contract address ?",
-    text: "",
+    title: "Is there a roadmap for future development",
+    text: "The future of PetZ NFTs might involve exciting developments like in-game functionalities, breeding mechanics, or even integration with other platforms.  Stay tuned to official PetZ NFT channels for any roadmap announcements.",
   },
 ];
