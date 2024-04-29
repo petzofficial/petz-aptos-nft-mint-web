7697109ee4b356c51a475a2fb03197b191bdde29
Yong Loon <nyl9488.yln@gmail.com>
2024-04-29 17:01:43 +0800

    updates

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,7 +3,7 @@ import GlobalStyles from "../assets/styles/GlobalStyles";
 import Layout from "../common/layout";
 import Header from "../components/section/header/v1/Header";
 import PageHeader from "../common/pageHeader/v2";
-import About from "../components/section/about/v6";
+//import About from "../components/section/about/v6";
 import HowToMint from "../components/section/howToMint/v3";
 import Counter from "../components/section/counter/v2";
 import Team from "../components/section/team/v6";
@@ -22,7 +22,7 @@ const AboutPage = () => {
         {walletModalvisibility && <WalletModal />}
         <Header />
         <PageHeader />
-        <About />
+       {/*  <About /> */}
         <HowToMint />
         <Counter />
         <Team />
